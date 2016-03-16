9f29e6d4b3b748400c7e14f593951935dbf444bb
Samuel Ramos Barroso <samuelrb1@gmail.com>
2016-03-16 19:25:46 +0000

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,54 @@
+var C1 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/c1.mp3');
+  audio.play()
+}
+
+var B1 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/b1.mp3');
+  audio.play()
+}
+var A1 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/a1.mp3');
+  audio.play()
+}
+var D1 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/d1.mp3');
+  audio.play()
+}
+var E1 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/e1.mp3');
+  audio.play()
+}
+var F1 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/f1.mp3');
+  audio.play()
+}
+var G1 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/g1.mp3');
+  audio.play()
+}
+var C2 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/c2.mp3');
+  audio.play()
+}
+
+var C1s = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/c1s.mp3');
+  audio.play()
+}
+var D1s = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/d1s.mp3');
+  audio.play()
+}
+var F1s = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/f1s.mp3');
+  audio.play()
+}
+var G1s = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/g1s.mp3');
+  audio.play()
+}
+var A1s = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/a1s.mp3');
+  audio.play()
+}
